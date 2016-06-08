d356095d390e4ba20f847fa650bde81bd9ff0c37
Oussama El Bacha <oussama.elbacha@gmail.com>
2016-06-08 23:24:55 +0000

    First Upload

---
+++ android/src/main/java/com/odinvt/portavailable/PortAvailableModule.java
@@ -0,0 +1,70 @@
+package com.odinvt.portavailable;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+
+public class PortAvailableModule extends ReactContextBaseJavaModule {
+    private static final int MIN_PORT_NUMBER = 0;
+    private static final int MAX_PORT_NUMBER = 0xFFFF;
+
+    public PortAvailableModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "RNPortAvailable";
+    }
+
+    @ReactMethod
+    public void available(int port, Promise promise) {
+        boolean available;
+        try {
+            available = check(port);
+
+            promise.resolve(available);
+        } catch (Exception e) {
+            promise.reject(e);
+        }
+    }
+
+    public boolean check(int port) throws IllegalArgumentException, IOException {
+        if (port < MIN_PORT_NUMBER || port > MAX_PORT_NUMBER) {
+            throw new IllegalArgumentException("Invalid start port: " + port);
+        }
+
+        ServerSocket ss = null;
+        DatagramSocket ds = null;
+        boolean result = false;
+        try {
+            ss = new ServerSocket(port);
+            ss.setReuseAddress(true);
+            ds = new DatagramSocket(port);
+            ds.setReuseAddress(true);
+            result = true;
+        } catch (IOException e) {
+            throw new IOException(e);
+        } finally {
+            if (ds != null) {
+                ds.close();
+            }
+
+            if (ss != null) {
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    /* should not be thrown */
+                    throw new IOException(e);
+                }
+            }
+        }
+
+        return result;
+    }
+}
